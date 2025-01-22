eb6ceabeb8b78652c0ece99a88f1ee932985f0c9
Idan Attias <idanat@google.com>
2025-01-22 21:47:09 +0200

    get correction with ai

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 A `thefuck` alternative written in Typescript with Deno.
 
 ## Compile
+
 `deno compile --allow-env --allow-read --allow-run main.ts`
--- main.ts
+++ main.ts
@@ -1,17 +1,32 @@
 import { getLastCommand } from "./history.ts";
+import { GoogleGenerativeAI } from "npm:@google/generative-ai";
+import process from "node:process";
 
 async function main() {
-  console.log(`Last command: ${await getLastCommand()}`);
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
 
-  const correctedCommand = "echo";
-  const args = ["1", "2"];
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  const lastCommand = await getLastCommand();
+  console.log(`Last command: ${lastCommand}`);
+
+  const response = await model.generateContent(
+    `I had a type in the command "${lastCommand}".
+Can you please provide me with the correct command? I want you to output a plain json with two fields:
+"command" for the first part of the command: probably a binary name, and certainly should not contain s apce, and "args" as an array of arguments. Do not include any other text in the response, including any markdown hints. The response should start with "{"`,
+  );
+  // const correctedCommand = "echo";
+  // const args = ["1", "2"];
+
+  const x = JSON.parse(response.response.text());
+  const correctedCommand = x.command;
+  const args = x.args;
 
   if (correctedCommand) {
     const command = new Deno.Command(correctedCommand, {
       args,
     });
     const stdout = (await command.output()).stdout;
-    new TextDecoder().decode(stdout);
     console.log(new TextDecoder().decode(stdout));
   } else {
     console.log("Skipped.");
