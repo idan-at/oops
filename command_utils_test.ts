f273445d22ac5bfcc5b763d71a40555af99a230f
Idan Attias <idanzii@gmail.com>
2025-01-23 23:00:59 +0200

    tests for getCommandOoutput (#3)

---
+++ command_utils_test.ts
@@ -0,0 +1,14 @@
+import { expect } from "jsr:@std/expect";
+import { getCommandOoutput } from "./command_utils.ts";
+
+Deno.test("getCommandOoutput stdout", async () => {
+  const { stdout, stderr } = await getCommandOoutput("echo 1");
+  expect(stdout).toBe("1\n");
+  expect(stderr).toBe("");
+});
+
+Deno.test("getCommandOoutput stderr", async () => {
+  const { stdout, stderr } = await getCommandOoutput("ls does-not-exist");
+  expect(stdout).toBe("");
+  expect(stderr).toMatch(/No such file or directory/);
+});
