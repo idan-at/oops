1a4a57274c50e119dbdc92ee05552219484291a9
Idan Attias <idanat@google.com>
2025-01-24 15:07:43 +0200

    first integration test

--- logger.ts
+++ logger.ts
@@ -1,3 +1,7 @@
 import { Logger } from "jsr:@deno-library/logger";
 
 export const logger = new Logger();
+
+if (Deno.env.get("OOPS_DEBUG") == null) {
+  logger.disable("debug");
+}
--- main_test.ts
+++ main_test.ts
@@ -1,5 +1,26 @@
-import { assertEquals } from "@std/assert";
+import { expect } from "jsr:@std/expect";
 
-Deno.test(function addTest() {
-  assertEquals(1, 1);
+const decoder = new TextDecoder();
+
+await new Deno.Command("deno", {
+  args: [
+    "compile",
+    "--allow-env",
+    "--allow-read",
+    "--allow-run",
+    "--allow-net",
+    "--allow-sys",
+    "main.ts",
+  ],
+}).spawn().status;
+
+Deno.test("help menu", async () => {
+  const { stdout, success } = await new Deno.Command("./oops", {
+    args: ["--help"],
+  }).output();
+
+  expect(success).toBeTruthy();
+  expect(decoder.decode(stdout)).toEqual(
+    "--ai for gemini based suggestions.\n",
+  );
 });
